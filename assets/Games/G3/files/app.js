8820c8f5ea4f5197f5f080d9183e5d3baa63432c
Dani-8 <daniyalw6190@gmail.com>
2025-08-21 01:29:18 +0500

    minor updates

--- assets/Games/G3/files/app.js
+++ assets/Games/G3/files/app.js
@@ -1,7 +1,8 @@
 let userScore = document.getElementById("user-score")
 let compScore = document.getElementById("comp-score")
 let round = document.getElementById("round")
-
+let userAttempt = document.getElementById("user-attempt")
+let compAttempt = document.getElementById("comp-attempt")
 
 
 
