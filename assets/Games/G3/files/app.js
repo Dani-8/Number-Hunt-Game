40766a1fd3218e2126d664e94d6bda62b6b3377c
Dani-8 <daniyalw6190@gmail.com>
2025-08-23 23:09:50 +0500

    mminor updates

--- assets/Games/G3/files/app.js
+++ assets/Games/G3/files/app.js
@@ -4,10 +4,10 @@ let compScore = 0
 let userAttempts = 0
 let compAttempts = 0
 
-let targetNumber = 0
-
-let roundOver = false
 let currentRound = 0
+let roundOver = false
+
+let targetNumber = 0
 
 let minRange = 1
 let maxRange = 5
@@ -23,7 +23,7 @@ let roundDisplay = document.getElementById("round")
 let rangeMsgDisplay = document.getElementById("range-msg")
 let messageBox = document.getElementById("msg")
 
-let userGuess = document.getElementById("user-input")
+let userGuessInput = document.getElementById("user-input")
 let guessBtn = document.getElementById("guess-btn")
 let newGameBtn = document.getElementById("new-game-btn")
 
@@ -32,14 +32,58 @@ let newGameBtn = document.getElementById("new-game-btn")
 
 
 
+/**
+ * STARTS A NEW GAME
+ * RESETS SCORES, ROUNDS,& BEGINS THE NEW ROUND
+*/ 
+function startNewGame() {
+    userScore = 0
+    compScore = 0
+    currentRound = 0
+
+}
+
+
+/**
+ * STARTS A NEW ROUND OF THE GAME
+ * SETS NEW RANGE, RESETS ATTEMPTS & GENERATES A NEW TARGET NUMBER
+ */
+
+function startNewGame(){
+    currentRound++
+    roundOver = false
+    userAttempts = 3
+    compAttempts = 3
+    userGuessInput.value = ""         //clear Previous Guess
+    userGuessInput.focus()           //Focus on Input Field for quick Entry
+
+
+    //DETERMINE RNAGE FOR EACH ROUND
+    if (currentRound === 1){
+        minRange = 1
+        maxRange = 5
+    }else if(currentRound === 2){
+        minRange = 5
+        maxRange = 10
+    }else if(currentRound === 3){
+        minRange = 1
+        maxRange = 10
+    }else{
+        console.error("Invalid Round");
+        return
+    }
+
+}
 
 
 
+/**
+ * HANDLE THE USER'S & COMPUTER'S GUESSES
+ */
+function handleGuess() {
+    
 
-
-
-
-
+}
 
 
 
