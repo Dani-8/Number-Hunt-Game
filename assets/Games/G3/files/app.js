1369f51cbb5cca592085a7369ded3f5cfbde6d02
Dani-8 <daniyalw6190@gmail.com>
2025-08-23 22:50:43 +0500

    minor udpates

--- assets/Games/G3/files/app.js
+++ assets/Games/G3/files/app.js
@@ -1,11 +1,31 @@
-let userScore = document.getElementById("user-score")
-let compScore = document.getElementById("comp-score")
-let round = document.getElementById("round")
-let userAttempt = document.getElementById("user-attempt")
-let compAttempt = document.getElementById("comp-attempt")
+// GAME VARIABLES
+let userScore = 0
+let compScore = 0
+let userAttempts = 0
+let compAttempts = 0
+
+let targetNumber = 0
+
+let roundOver = false
+let currentRound = 0
+
+let minRange = 1
+let maxRange = 5
 
 
+// DISPLAY ELEMENTS
+let userScoreDisplay = document.getElementById("user-score")
+let compScoreDisplay = document.getElementById("comp-score")
+let userAttemptDisplay = document.getElementById("user-attempt")
+let compAttemptDisplay = document.getElementById("comp-attempt")
 
+let roundDisplay = document.getElementById("round")
+let rangeMsgDisplay = document.getElementById("range-msg")
+let messageBox = document.getElementById("msg")
+
+let userGuess = document.getElementById("user-input")
+let guessBtn = document.getElementById("guess-btn")
+let newGameBtn = document.getElementById("new-game-btn")
 
 
 
